5ecadabdfa6be2d9d8bb8a822d8c017059774699
frey-norden <frey.norden@icloud.com>
2020-04-29 19:22:26 +0300

    iteratively following lec3

--- lec3.py
+++ lec3.py
@@ -26,3 +26,28 @@ x = 10
 for i in range(1,x):
     if x%i == 0:
         print('divisor ',i)
+
+print('\n')
+x = 1515361
+if x>=0:
+    for ans in range(1,x):
+        if ans*ans == x:
+            print(ans)
+            break
+
+print('\n')
+x = 100
+divisors = ()
+for i in range(1, x):
+    if x%i == 0:
+        divisors = divisors + (i,)
+print(divisors)
+
+print('\n')
+# strings as ordered sequence of characters
+s1 = 'abcdefg'
+s2 = 'hijklmn'
+
+print(s1 + s2)
+
+print(s1)
