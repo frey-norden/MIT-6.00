990f5ed7d3ce85c306e528abde4eacfbbc644879
frey-norden <frey.norden@icloud.com>
2020-05-13 20:29:58 +0300

    typo in totLegs var

--- solve2.py
+++ solve2.py
@@ -6,7 +6,7 @@ def solve2(numLegs, numHeads):
     for numSpiders in range(0, numHeads + 1):
         for numChicks in range(0, numHeads - numSpiders + 1):
             numPigs = numHeads - numChicks - numSpiders
-            totlegs = 4*numPigs + 2*numChicks + 8*numSpiders
+            totLegs = 4*numPigs + 2*numChicks + 8*numSpiders
             if totLegs == numLegs:
                 return [numPigs, numChicks, numSpiders]
                 solutionFound = True
@@ -15,9 +15,16 @@ def solve2(numLegs, numHeads):
 def barnYard():
     heads = int(input('Enter number of heads: '))
     legs = int(input('Enter number of legs: '))
+    pigs, chickens, spiders = solve2(legs, heads)
+    if pigs == None:
+        print('There is no solution')
+    else:
+        print('Number of pigs:', pigs)
+        print('Number of chickens:', chickens)
+        print('Number of spiders:', spiders)
 
 def main():
-    solve2(numLegs, numHeads)
+    barnYard()
 
 if __name__ == '__main__':
     main()
