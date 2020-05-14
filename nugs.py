20bd298348d4fa155ea2985808364ccb0cd4abbb
frey-norden <frey.norden@icloud.com>
2020-05-14 09:41:39 +0300

    added cashier function

--- nugs.py
+++ nugs.py
@@ -7,7 +7,6 @@ def mcNuggets(a, b, c, total):
     solutions = list()
     solutionFound = False
     x = 6*a + 9*b + 20*c
-
     if x == total:
         #return [a, b, c]
         print('solution',[a, b, c], total)
@@ -19,8 +18,17 @@ def mcNuggets(a, b, c, total):
     #if not solutionFound: print('no solution')
     #print(solutions)
 
-for x in range(0,10):
-    for y in range(0,10):
-        for z in range(0,6):
-            for total in range(50,101):
-                mcNuggets(x, y, z, total)
+def cashier():
+    u = int(input('Enter upper limit of nuggets: '))
+    l = int(input('Enter lower limit of nuggets: '))
+    for x in range(0,int(u/6+1)):
+        for y in range(0,int(u/9+1)):
+            for z in range(0,int(u/20+1)):
+                for total in range(l,u+1):
+                    mcNuggets(x, y, z, total)
+
+def main():
+    cashier()
+
+if __name__ == '__main__':
+    main()
